2a038ccd7a59648aacbe6902b5778166a8d7e846
lioraro21 <lioraro206@gmail.com>
2023-11-28 16:23:31 +0200

    adding divide

--- app.py
+++ app.py
@@ -4,7 +4,9 @@ n2 = int(input("Enter second number "))
 sum = n1 + n2
 sub = n1 - n2
 multiply = n1 * n2
+divide = n1 / n2
 
 print(f"{n1} + {n2} = {sum}")
 print(f"{n1} - {n2} = {sub}")
 print(f"{n1} * {n2} = {multiply}")
+print(f"{n1} / {n2} = {divide}")
